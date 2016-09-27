120cd7d4b4751555467d69a716845673b56bd25a
caleron <paddy.buchhardt@arcor.de>
2016-09-27 19:28:41 +0200

    Kommentare

--- src/com/awaker/config/ConfigChangeListener.java
+++ src/com/awaker/config/ConfigChangeListener.java
@@ -2,5 +2,11 @@ package com.awaker.config;
 
 @FunctionalInterface
 public interface ConfigChangeListener {
+    /**
+     * Wird ausgelöst, wenn sich eine Konfiguration ändert, für die sich ein Objekt mit Config.addListener
+     * registriert hat.
+     *
+     * @param key Der Schlüssel, von dem sich der Wert geändert hat.
+     */
     void configChanged(ConfigKey key);
 }
