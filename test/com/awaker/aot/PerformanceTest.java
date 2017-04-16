d753338f2e4cc8811aa52ca7e94f71ae3fbb7c38
caleron <paddy.buchhardt@arcor.de>
2017-04-16 23:22:26 +0200

    added performance test to compare single threaded with multi threaded ahead of time analysis

---
+++ test/com/awaker/aot/PerformanceTest.java
@@ -0,0 +1,44 @@
+package com.awaker.aot;
+
+import com.awaker.analyzer.aot.PreAnalyzer;
+import com.awaker.analyzer.aot.ThreadedAotAnalyzer;
+import com.awaker.data.MediaManager;
+import com.awaker.data.TrackWrapper;
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.util.Date;
+
+/**
+ * Created by Patrick on 16.04.2017.
+ */
+public class PerformanceTest {
+    //around 10.5s
+    @Test
+    public void testSingleThread() {
+        TrackWrapper track = new TrackWrapper(0, "", "", "", "media\\N.W.A - Straight Outta Compton.mp3", new Date(), 259);
+
+        InputStream stream = MediaManager.getFileStream(track);
+        PreAnalyzer preAnalyzer = new PreAnalyzer();
+        long start = System.currentTimeMillis();
+        preAnalyzer.analyze(stream);
+
+        System.out.println((System.currentTimeMillis() - start));
+    }
+
+    //around 2.9s
+    @Test
+    public void testMultiThread() {
+
+        TrackWrapper track = new TrackWrapper(0, "", "", "", "media\\N.W.A - Straight Outta Compton.mp3", new Date(), 259);
+
+        InputStream stream = MediaManager.getFileStream(track);
+        ThreadedAotAnalyzer preAnalyzer = new ThreadedAotAnalyzer();
+
+        long start = System.currentTimeMillis();
+
+        preAnalyzer.analyze(stream);
+
+        System.out.println((System.currentTimeMillis() - start));
+    }
+}
