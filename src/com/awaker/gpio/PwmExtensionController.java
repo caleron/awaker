bb93e9181d68567b5677a95f7a1bbf1f7de85e70
caleron <paddy.buchhardt@arcor.de>
2017-05-26 21:46:13 +0200

    made the PwmExtensionController work

--- src/com/awaker/gpio/PwmExtensionController.java
+++ src/com/awaker/gpio/PwmExtensionController.java
@@ -2,6 +2,9 @@ package com.awaker.gpio;
 
 import com.pi4j.gpio.extension.pca.PCA9685GpioProvider;
 import com.pi4j.gpio.extension.pca.PCA9685Pin;
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinPwmOutput;
 import com.pi4j.io.gpio.Pin;
 import com.pi4j.io.i2c.I2CBus;
 import com.pi4j.io.i2c.I2CFactory;
@@ -10,6 +13,14 @@ import java.io.IOException;
 import java.math.BigDecimal;
 
 public class PwmExtensionController {
+    //min/max step count to use at 50Hz
+    private static final int ABSOLUTE_MINIMUM = 155;
+    private static final int ABSOLUTE_MAXIMUM = 650;
+
+    //bounds to use
+    private static final int MINIMUM_STEPS = 155;
+    private static final int MAXIMUM_STEPS = 500;
+
     private static PwmExtensionController instance = null;
     private PCA9685GpioProvider provider;
 
@@ -17,7 +28,12 @@ public class PwmExtensionController {
         try {
             //gibt wohl nur bus 1 auf raspberry
             //50 hertz setzen
-            provider = new PCA9685GpioProvider(I2CFactory.getInstance(I2CBus.BUS_1), 1, new BigDecimal(50));
+            I2CBus bus = I2CFactory.getInstance(I2CBus.BUS_1);
+            provider = new PCA9685GpioProvider(bus, 0x40, new BigDecimal("50"));
+            provisionPwmOutputs(provider);
+
+            // Reset outputs
+            provider.reset();
         } catch (I2CFactory.UnsupportedBusNumberException | IOException e) {
             e.printStackTrace();
         }
@@ -36,18 +52,16 @@ public class PwmExtensionController {
      * @param degree from -90 to +90
      */
     public void setServoPosition(Pin pin, short degree) {
-        double min = 0.05;
-        double max = 0.1;
-        //now scale between min and max
-        double power = (min + 0.025) + (degree / 90.0) * 0.025;
-        //12 bit pwm = 4096 steps = 0 ... 4095 as pwm value
-        double steps = 4095;
-        //thats the step where the pin should turn off, between 0 and 4095 inclusively
-        long offPosition = Math.round(power * steps);
-        //cap for security
-        offPosition = (long) Math.max(Math.min(steps, offPosition), 0);
-        //now with onPosition = 0, the pwm on to off ratio equals to the factor
-        provider.setPwm(pin, 0, (int) offPosition);
+        //middle step count
+        int middleSteps = (int) ((MAXIMUM_STEPS + MINIMUM_STEPS) / 2.0);
+        //scale between min and max
+        int margin = MAXIMUM_STEPS - middleSteps;
+        int offPosition = (int) (middleSteps + ((degree / 90.0) * margin));
+
+        //ensure off position is in range
+        offPosition = Math.max(MINIMUM_STEPS, Math.min(MAXIMUM_STEPS, offPosition));
+
+        provider.setPwm(pin, 0, offPosition);
     }
 
     public static void init() {
@@ -57,4 +71,15 @@ public class PwmExtensionController {
         instance = new PwmExtensionController();
     }
 
+    private static GpioPinPwmOutput[] provisionPwmOutputs(final PCA9685GpioProvider gpioProvider) {
+        GpioController gpio = GpioFactory.getInstance();
+        GpioPinPwmOutput myOutputs[] = {
+                gpio.provisionPwmOutputPin(gpioProvider, PCA9685Pin.PWM_00, "Pulse 00"),
+                gpio.provisionPwmOutputPin(gpioProvider, PCA9685Pin.PWM_01, "Pulse 01"),
+                gpio.provisionPwmOutputPin(gpioProvider, PCA9685Pin.PWM_02, "Pulse 02"),
+                gpio.provisionPwmOutputPin(gpioProvider, PCA9685Pin.PWM_03, "Pulse 03"),
+                gpio.provisionPwmOutputPin(gpioProvider, PCA9685Pin.PWM_04, "Pulse 04"),
+                gpio.provisionPwmOutputPin(gpioProvider, PCA9685Pin.PWM_05, "Pulse 05")};
+        return myOutputs;
+    }
 }
